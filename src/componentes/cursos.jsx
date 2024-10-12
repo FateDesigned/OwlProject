fe3b52af76a3cfc776da76b64195557db976c42c
Dilan-RedCode <dilanchaverra1@gmail.com>
2024-10-12 01:06:45 -0500

    Cursos

--- src/componentes/cursos.jsx
+++ src/componentes/cursos.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // styles
-import './styles/cursos.css'; 
+import './styles/cursos.css';
 
 const Cursos = () => {
 
@@ -8,12 +8,21 @@ const Cursos = () => {
 
 
   return (
-    <div className="container">
-      <div className="cursos">
-        <h1>Contenido principal</h1>
-        <p>Este es el contenido de la página principal.</p>
+    <main className="Cursos">
+        <h3 className="tittleCursos" id='textCursos'>Mis Cursos</h3>
+      <div className="boxCursos">
+
+
+        <div  className="cursos" id='curso1'>
+
+          <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2" />
+          </svg>
+
+          <h3 className="nuevoCurso">Agregar curso</h3>
+        </div>
       </div>
-    </div>
+    </main>
   );
 };
 
